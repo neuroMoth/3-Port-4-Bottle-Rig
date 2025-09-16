8bfc0bd86e75ea630d965a1c64ed3cc023b3bb64
Blake <48300180+blakehourigan@users.noreply.github.com>
2025-09-16 11:43:25 -0400

    remove unused alternation files

--- Code/MATLAB and BPOD/Bpod Local/Protocols/alternation_day_1_8_nonconsecutive_WATER_right_ODOR_left/stop_experiment.m
+++
@@ -1,8 +0,0 @@
-function stop_experiment(A, W)
-    A.scope_StartStop;
-    A.endAcq; % Close Oscope GUI
-    A.stopReportingEvents; % Stop sending events to state machine
-    clear A
-    clear W
-    return
-end
