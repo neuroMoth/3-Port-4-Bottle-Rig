68084cfcb1c07e92c9ce1a02e714908770bf9d4a
Timothy Vladimir Dong <tvdong01@louisville.edu>
2025-09-30 13:02:22 -0400

    Protocol for a new version of testing
    To test on the Bpod. Should save late licks to the correct trial and report incorrect vs disengaged.

---
+++ Code/MATLAB and BPOD/Bpod Local/Protocols/testing2_WATER_left_ODOR_right/CenterPort.m
@@ -0,0 +1,49 @@
+    classdef CenterPort
+        properties 
+            left_valve; % the valve in the center is not a constant. 
+            left_valve_time; % the delay time for this valve 
+
+            right_valve; % the valve in the center is not a constant. 
+            right_valve_time; % the delay time for this valve 
+        end
+        properties (Constant)
+            LEFT_COUNTER_ID = 1;
+            RIGHT_COUNTER_ID = 2;
+
+            LEFT_LICK_INPUT = 'AnalogIn1_1';
+            RIGHT_LICK_INPUT = 'AnalogIn1_2'; % input for the second spout in the center port.
+            DOOR = 'Flex2DO';
+            ACTIVE_SPOUT = 'Flex4DO';
+
+            LEFT_COUNTER_EVENT = 'GlobalCounter1_End';
+            RIGHT_COUNTER_EVENT = 'GlobalCounter2_End';
+
+            WATER_VALVE = 2;
+            ODOR_VALVE = 5;
+        end 
+        methods
+            function obj = setValve(obj, side, valve_number)
+                global BpodSystem
+
+                time_variable_name = sprintf('open_time_%d', valve_number);
+                if (side == 3)
+                    obj.right_valve = valve_number;
+
+                    obj.right_valve_time = BpodSystem.ProtocolSettings.GUI.(time_variable_name)/1000;
+                elseif (side == 1)
+                    obj.left_valve = valve_number; 
+
+                    obj.left_valve_time = BpodSystem.ProtocolSettings.GUI.(time_variable_name)/1000;
+                else
+                end
+            end
+            function obj = switchLeftValve(obj)
+                switch (obj.left_valve)
+                    case 2
+                        obj.left_valve = 5;
+                    case 5 
+                        obj.left_valve = 2;
+                end
+            end
+        end
+    end
