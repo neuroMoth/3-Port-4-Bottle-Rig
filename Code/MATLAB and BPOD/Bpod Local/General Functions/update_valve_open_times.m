8947fc3191f94208ad7630528ef5d1c93119a3f4
neuroMoth <tvdong01@louisville.edu>
2026-07-01 11:49:17 -0400

    Reorganizing older protocols, update to valve timing function, and a prototype protocol for vac and rinse

--- Code/MATLAB and BPOD/Bpod Local/General Functions/update_valve_open_times.m
+++ Code/MATLAB and BPOD/Bpod Local/General Functions/update_valve_open_times.m
@@ -1,4 +1,4 @@
-function settings_struct = update_valve_open_times(settings_struct, valve_labels)
+function settings_struct = update_valve_open_times(settings_struct, valve_labels, target_amount)
 % set_open_valve_values sets open times for valves based on duration type.
 %
 % Inputs:
@@ -11,16 +11,17 @@ function settings_struct = update_valve_open_times(settings_struct, valve_labels
 arguments
     settings_struct struct
     valve_labels double
+    target_amount double
 end
 
 for i = 1:length(valve_labels)
     valve = valve_labels(i);
 
     variable_name =  sprintf('select_amount_valve%d', valve);
-    targetAmounts = str2double(settings_struct.GUIMeta.(variable_name).String);
+    %targetAmounts = str2double(settings_struct.GUIMeta.(variable_name).String);
 
     open_time = sprintf('open_time_%d', valve);
-    settings_struct.GUI.(open_time) = (GetValveTimes(targetAmounts(settings_struct.GUI.(variable_name)),valve)) * 1000;
+    settings_struct.GUI.(open_time) = (GetValveTimes(target_amount,valve)) * 1000;
 end
 
 end
---
+++ Code/MATLAB and BPOD/Bpod Local/Protocols/testing_v3_rinseSharedChannel/ExperimentVariables.m
@@ -0,0 +1,48 @@
+classdef ExperimentVariables 
+    properties (Constant)
+        TOTAL_ALLOWED_TIME = 4000; %seconds
+        ITI_TIME = 15; %seconds 
+        ITI_ENDTIME = 2; %portion of ITI for the end of the trial (taken from the whole ITI duration)
+        PUNISHMENT_TIME = 10; %seconds
+        TTC_CENTER_TIME = 5; %seconds
+        TTC_LATERAL_TIME = 5; %seconds
+        LICK_WINDOW = 2; %seconds | Defines amount of seconds rat has to complete required amount of licks.
+        DELAY_TIME = 3; %seconds | delay from closing the center door to opening the lateral door
+        STIMULUS_WINDOW = 300/1000; %ms after valve closes before door goes up (if still within LICK_WINDOW)
+
+        VALVE_SET1 = [2, 3]; % Water valves
+        VALVE_SET2 = [5, 6]; % Odor valves
+        RINSE_VALVE = 7; 
+        VAC_VALVE = 'BNC1'; 
+        
+        REWARD_LICKS = [4 5 6]; %range of possible rewarded lick to be pseudorandomized per valve (+1 from # of dry licks)
+        REWARD_VALVE_DELAY = 0; %delay from lick detection to valve opening
+        
+        STIM_VOLUME = 10; 
+        LOAD_VOLUME = 400; % Should be approximately 5x the dead space of the manifold (current estimate about 80ul)
+        RINSE_VOLUME = 1500; % Currently 1500ul or 1.5ml. 
+ 
+        MAXIMUM_TRIALS = 160; 
+        MINIMUM_TRIALS = 160; 
+        TRIALS_PER_BLOCK = 20; 
+        MAX_REPEATS = 4; 
+
+        %CONDITION_CODE = 'WROL'; % WROL = Water Right, Odor Left  
+        %SKIPPED_TRIALS_THRESHOLD = 20; % threshold of consecutive trials skipped in a 20 trial block to end early
+        %CORRECT_REQUIRED_TO_SWITCH = 2; % only for use with alternation training days 
+
+        % these varaibles are used to make door commands more intuitive and easy to understand & read.
+        UP = 0;
+        DOWN = 1;
+        LEFT_SPOUT = 0;
+        RIGHT_SPOUT = 1;
+
+        ITI_TIMER_ID = 1; 
+        LICK_WINDOW_TIMER_ID = 2;
+        STIM_WINDOW_TIMER_ID = 3; 
+        
+        itiTimeExpired = 'GlobalTimer1_End';
+        lickTimeExpired = 'GlobalTimer2_End';
+        stimTimeExpired = 'GlobalTimer3_End';
+    end
+end
