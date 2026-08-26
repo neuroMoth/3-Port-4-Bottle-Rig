3132806f7b86380f579619ec65e45d73007113f3
neuroMoth <tvdong01@louisville.edu>
2026-08-26 14:24:43 -0400

    Adding 1st attempt at new rinse protocol with gas clearing (v3.5). Moving old testing protocol w rinse.
    Old v3 was the first attempt of having a shared channel with water rinse (spout connected to a multi-channel manifold). This new protocol is for use with a new manifold with check valves to allow for a gas clearing step (also with new FFKM sealed valves).

---
+++ Code/MATLAB and BPOD/Bpod Local/Protocols/testing_v3/ExperimentVariables.m
@@ -0,0 +1,49 @@
+classdef ExperimentVariables 
+    properties (Constant)
+        % All time parameters (in seconds)
+        TOTAL_ALLOWED_TIME = 4200;
+        ITI_TIME = 15; 
+        ITI_ENDTIME = 2; % portion of ITI for the end of the trial (subtracted from whole ITI duration)
+        PUNISHMENT_TIME = 10; 
+        TTC_CENTER_TIME = 5; 
+        TTC_LATERAL_TIME = 5; 
+        LICK_WINDOW = 2; % time rat has to complete required amount of licks.
+        DELAY_TIME = 3; % delay from closing the center door to opening the lateral door
+        STIMULUS_WINDOW = 0.05; % pause after final valve closes before door goes up (if still within LICK_WINDOW)
+        GAS_TIME = 1; % time for gas purge
+
+        VALVE_SET1 = [2, 3]; % Water valves
+        VALVE_SET2 = [5, 6]; % Odor valves
+        RINSE_VALVE = 7; 
+        GAS_VALVE = 'BNC1'; 
+        
+        REWARD_LICKS = [4 5 6]; %range of possible rewarded lick to be pseudorandomized per valve (+1 from # of dry licks)
+        REWARD_VALVE_DELAY = 0; %delay from lick detection to valve opening
+        
+        STIM_VOLUME = 10; 
+        PRIMING_VOLUME = 50; % 0.5x dead space of the manifold (current estimate about 100ul). For "seasoning" step. 
+        LOAD_VOLUME = 250; % 2.5x dead space of the manifold. Fills the manifold and pushes the first amount to waste. 
+        RINSE_VOLUME = 500; % 500ul (0.5ml) for each rinse round (2 rounds with gas clearing in between). 
+ 
+        MAXIMUM_TRIALS = 160; 
+        MINIMUM_TRIALS = 160; 
+        TRIALS_PER_BLOCK = 20; 
+        MAX_REPEATS = 4; 
+
+        %CONDITION_CODE = 'WROL'; % WROL = Water Right, Odor Left  
+        %SKIPPED_TRIALS_THRESHOLD = 20; % threshold of consecutive trials skipped in a 20 trial block to end early
+        %CORRECT_REQUIRED_TO_SWITCH = 2; % only for use with alternation training days 
+
+        % these varaibles are used to make door commands more intuitive and easy to understand & read.
+        UP = 0;
+        DOWN = 1;
+        LEFT_SPOUT = 0;
+        RIGHT_SPOUT = 1;
+
+        ITI_TIMER_ID = 1; 
+        LICK_WINDOW_TIMER_ID = 2;
+        
+        itiTimeExpired = 'GlobalTimer1_End';
+        lickTimeExpired = 'GlobalTimer2_End';
+    end
+end
